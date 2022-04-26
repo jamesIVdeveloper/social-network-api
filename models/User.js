0e5e40de4ff3c070f721ed435966c9128b110c75
James Murphy <jamesivdeveloper@gmail.com>
2022-04-25 22:37:04 -0500

    add user model and CRUD operations

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+      message: "Please enter a valid email",
+    },
+  },
+  thoughts: [],
+  friends: [],
+});
+
+// get friend count
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.reduce((total, friends) => total + friends.length + 1, 0);
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
